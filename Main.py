3fa410852c444a8e607e68820c6b8ab354cb7ebf
irfanessa2 <irfanessa@hotmail.co.uk/bin/python3>
2023-12-26 23:06:27 +0000

    Sample images, software shows fps. improved otimisation

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
 import cv2
 import threading
 import queue
+import time
 
 class CameraStream:
     def __init__(self, src):
         self.cap = cv2.VideoCapture(src)
-        self.q = queue.Queue()
+        self.q = queue.Queue(1)
         self.stopped = False
 
         if not self.cap.isOpened():
@@ -45,34 +46,53 @@ class CameraStream:
         self.cap.release()
 
 def main():
-    camera_url = 'rtsp://irfanessa:Locker123@192.168.1.156/stream1'
-    stream = CameraStream(camera_url)
+    camera_url = 'rtsp://192.168.1.88:8554/c920'
+    #stream = CameraStream(camera_url)
+    cap = cv2.VideoCapture(camera_url)
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    try:
-        while True:
-            if stream.more():
-                frame = stream.read()
+    prev_frame_time, new_frame_time = 0, 0
 
-                # Convert frame to grayscale for face detection
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    INTERLEAVE = 5 
+    frame_cnt = 0
+    faces = None
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-                # Detect faces in the frame
-                faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        orig = frame.copy()
+        new_frame_time = time.time()
 
-                # Draw green squares around detected faces
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # FPS calculation
+        fps = 1/(new_frame_time - prev_frame_time)
+        prev_frame_time = new_frame_time
+        fps_text = "{:.2f}".format(fps)  # Format FPS to two decimal places
 
-                # Display the captured frame
-                cv2.imshow('Tapo Camera Live', frame)
+        if frame_cnt < 1:
+            frame_cnt = INTERLEAVE
+            # Convert frame to grayscale for face detectionw
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                # Break the loop when 'q' is pressed
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-    finally:
-        stream.stop()
-        cv2.destroyAllWindows()
+            # Detect faces in the frame
+            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(orig, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                
+
+        # Put FPS text on frame
+        cv2.putText(orig, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        print(f"{fps:.2f}", end='\r', flush=True)
+
+        # Display the captured frame
+                
+        cv2.imshow('Tapo Camera Live', orig)
+        frame_cnt -= 1
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
