9bbb100dae81661dfe35d967f283c643ea72f772
irfanessa2 <irfanessa@hotmail.co.uk/bin/python3>
2024-01-03 22:00:53 +0000

    yawning implementation. change to face mask

--- testing_window.py
+++ testing_window.py
@@ -58,19 +58,8 @@ RIGHT_EYE_INDICES = [
 ]
 
 
-UPPER_LIP_INDICES = [
-    61, 
-    185, 
-    40, 
-    39, 
-    37, 
-    0, 
-    267, 
-    269, 
-    270, 
-    409, 
-    291]
-                                                                                # CHANGES
+UPPER_LIP_INDICES = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291]
+
 
 LOWER_LIP_INDICES = [
     146,
@@ -96,9 +85,10 @@ LOWER_LIP_INDICES = [
 class CameraStream(QThread):
     change_pixmap_signal = pyqtSignal(QImage)
     ear_signal = pyqtSignal(float)
+    mar_signal = pyqtSignal(float)
     eyes_closed_signal = pyqtSignal()
     eyes_open_signal = pyqtSignal()
-    latency = pyqtSignal(object)
+    latency_signal = pyqtSignal(object)
 
     def __init__(self, src=0, parent=None):
         super().__init__(parent)
@@ -110,68 +100,41 @@ class CameraStream(QThread):
         self.predictions = {}
         self.blinked = False
         self.opened = False
-        self.face_detection_thread = Thread()
+        self.face_feature_detection_thread = Thread()
         self.hand_detection_thread = Thread()
-        self.blink_detection_thread = Thread()
-        self.yawn_detection_thread = Thread()  # CHANGES
         self.face_results = None
         self.hand_results = None
-        self.blink_results = None
-        self.yawn_results = None    #CHANGES
         self.do_face = False
         self.do_hands = False
         self.do_blink = False
-        self.do_yawn = False    #CHANGES
+        self.do_yawn = False
 
     @pyqtSlot(bool)
     def set_do_hands(self, val):
         self.do_hands = val
-        if not val and self.hand_detection_thread.is_alive():
-            self.hand_detection_thread.join()  # blocking in MainThread, ensure the thread is very short
 
     @pyqtSlot(bool)
     def set_do_face(self, val):
         self.do_face = val
-        if not val and self.face_detection_thread.is_alive():
-            self.face_detection_thread.join()  # blocking in MainThread, ensure the thread is very short
-    
-    
+
     @pyqtSlot(bool)
     def set_do_yawn(self, val):
         self.do_yawn = val
-        if not val and self.yawn_detection_thread.is_alive():                                                   #CHANGES
-            self.yawn_detection_thread.join()  # blocking in MainThread, ensure the thread is very short        
-
 
     @pyqtSlot(bool)
     def set_do_blink(self, val):
         self.do_blink = val
-        if not val and self.blink_detection_thread.is_alive():
-            self.blink_detection_thread.join()  # blocking in MainThread, ensure the thread is very short
-
-
-    def face_detection_worker(self, frame):
-        # Face detection
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        face_cascade = cv2.CascadeClassifier(
-            cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-        )
-        self.face_results = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     def hand_detection_worker(self, frame):
         frame_rgb = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB)
         self.hand_results = self.hands.process(frame_rgb)
 
-    def blink_detection_worker(self, frame):
+    def face_feature_detection_worker(self, frame):
         # Blink detection
         frame_rgb = cv2.cvtColor(frame.copy(), cv2.COLOR_BGR2RGB)
-        self.blink_results = self.face_mesh.process(frame_rgb)
+        self.face_results = self.face_mesh.process(frame_rgb)
 
-    def draw_predictions(self, frame):
-        if self.face_results is not None:
-            for x, y, w, h in self.face_results:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+    def draw_hands(self, frame):
         # Hand detection
         if self.hand_results is not None and self.hand_results.multi_hand_landmarks:
             for hand_landmarks in self.hand_results.multi_hand_landmarks:
@@ -179,16 +142,60 @@ class CameraStream(QThread):
                     frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
                 )
 
-        if self.blink_results is not None and self.blink_results.multi_face_landmarks:
-            for face_landmarks in self.blink_results.multi_face_landmarks:
-                left_eye = self.get_eye_landmarks(
-                    face_landmarks.landmark, LEFT_EYE_INDICES
-                )
-                right_eye = self.get_eye_landmarks(
-                    face_landmarks.landmark, RIGHT_EYE_INDICES
-                )
+    def draw_face_features(self, frame):
+        # face detection
+        if self.face_results is not None and self.face_results.multi_face_landmarks:
+            for face_landmarks in self.face_results.multi_face_landmarks:
+                if self.do_face:
+                    for point in [
+                        np.array([lm.x, lm.y]) for lm in face_landmarks.landmark
+                    ]:
+                        cv2.circle(
+                            frame,
+                            (
+                                int(point[0] * frame.shape[1]),
+                                int(point[1] * frame.shape[0]),
+                            ),
+                            1,
+                            (0, 255, 0),
+                            -1,
+                        )
 
-                for point in left_eye + right_eye:
+                feature_points = []
+                if self.do_blink:
+                    left_eye = self.get_feature_landmarks(
+                        face_landmarks.landmark, LEFT_EYE_INDICES
+                    )
+                    right_eye = self.get_feature_landmarks(
+                        face_landmarks.landmark, RIGHT_EYE_INDICES
+                    )
+                    left_ear = self.calculate_ar(left_eye)
+                    right_ear = self.calculate_ar(right_eye)
+                    ear = (left_ear + right_ear) / 2.0
+                    self.ear_signal.emit(ear)
+
+                    feature_points = feature_points + right_eye + left_eye
+
+                    if ear < self.ear_threshold:
+                        if not self.blinked:
+                            self.blinked = True
+                            self.opened = False
+                            self.eyes_closed_signal.emit()
+                    elif not self.opened:
+                        self.opened = True
+                        self.blinked = False
+                        self.eyes_open_signal.emit()
+
+                if self.do_yawn:
+                    mouth = self.get_feature_landmarks(
+                        face_landmarks.landmark, UPPER_LIP_INDICES + LOWER_LIP_INDICES
+                    )
+                    mar = self.calculate_ar(mouth)
+                    self.mar_signal.emit(mar)
+
+                    feature_points = feature_points + mouth
+
+                for point in feature_points:
                     cv2.circle(
                         frame,
                         (
@@ -196,61 +203,39 @@ class CameraStream(QThread):
                             int(point[1] * frame.shape[0]),
                         ),
                         1,
-                        (0, 255, 0),
+                        (0, 0, 255),
                         -1,
                     )
 
-                left_ear = self.calculate_ear(left_eye)
-                right_ear = self.calculate_ear(right_eye)
-                ear = (left_ear + right_ear) / 2.0
-                self.ear_signal.emit(ear)
-
-                if ear < self.ear_threshold:
-                    if not self.blinked:
-                        self.blinked = True
-                        self.opened = False
-                        self.eyes_closed_signal.emit()
-                elif not self.opened:
-                    self.opened = True
-                    self.blinked = False
-                    self.eyes_open_signal.emit()
         return frame
 
     def run(self):
         cap = cv2.VideoCapture(self.src)
 
         while not self.stopped:
-            self.latency.emit(time.time_ns())
+            self.latency_signal.emit(time.time_ns())
             ret, frame = cap.read()
             if ret:
                 if self.do_hands:
-                    if not self.hand_detection_thread.is_alive():
-                        self.hand_detection_thread = Thread(
-                            target=self.hand_detection_worker, args=(frame.copy(),)
-                        )
-                        self.hand_detection_thread.start()
+                    self.service_thread(
+                        "hand_detection_thread",
+                        self.hand_detection_worker,
+                        (frame.copy(),),
+                    )
+                    self.draw_hands(frame)
                 else:
                     self.hand_results = None
 
-                if self.do_face:
-                    if not self.face_detection_thread.is_alive():
-                        self.face_detection_thread = Thread(
-                            target=self.face_detection_worker, args=(frame.copy(),)
-                        )
-                        self.face_detection_thread.start()
+                if self.do_face or self.do_blink or self.do_yawn:
+                    self.service_thread(
+                        "face_feature_detection_thread",
+                        self.face_feature_detection_worker,
+                        (frame.copy(),),
+                    )
+                    self.draw_face_features(frame)
                 else:
                     self.face_results = None
 
-                if self.do_blink:
-                    if not self.blink_detection_thread.is_alive():
-                        self.blink_detection_thread = Thread(
-                            target=self.blink_detection_worker, args=(frame.copy(),)
-                        )
-                        self.blink_detection_thread.start()
-                else:
-                    self.blink_results = None
-
-                frame = self.draw_predictions(frame)
                 qimg = self.convert_to_qimage(frame)
                 self.change_pixmap_signal.emit(qimg)
 
@@ -258,21 +243,29 @@ class CameraStream(QThread):
         self.hands.close()
         self.face_mesh.close()
 
+    def service_thread(self, thread, method, args=()):
+        if not getattr(self, thread).is_alive():
+            setattr(
+                self,
+                thread,
+                Thread(target=method, args=args, name=f"{method.__name__}"),
+            )
+            getattr(self, thread).start()
+
     def stop(self):
         self.stopped = True
         self.wait()
 
     @staticmethod
-    def calculate_ear(eye):
+    def calculate_ar(eye):
         min_x, min_y = np.min(eye, axis=0)
         max_x, max_y = np.max(eye, axis=0)
         return (max_y - min_y) / (max_x - min_x)
 
     @staticmethod
-    def get_eye_landmarks(landmarks, indices):
+    def get_feature_landmarks(landmarks, indices):
         return [np.array([landmarks[i].x, landmarks[i].y]) for i in indices]
-        # return [np.array([lm.x,lm.y]) for lm in landmarks] # uncomment for full face points
-
+    
     @staticmethod
     def convert_to_qimage(frame):
         h, w, ch = frame.shape
@@ -287,7 +280,7 @@ class TestingWindow(QWidget):
     do_face = pyqtSignal(bool)
     do_hands = pyqtSignal(bool)
     do_blink = pyqtSignal(bool)
-    do_yawn = pyqtSignal(bool)  # CHANGES
+    do_yawn = pyqtSignal(bool)
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -295,7 +288,7 @@ class TestingWindow(QWidget):
         self.resize(900, 480)
         self.blinks = 0
         self.ear = 0.0
-        self.yawns = 0  # CHANGES
+        self.yawns = 0
         self._last_frame_ts = 0
         self.fps = deque(maxlen=10)
         self.latency = deque(maxlen=10)
@@ -307,9 +300,11 @@ class TestingWindow(QWidget):
         self.eyes_opened_timer.setInterval(1)
         self.image_label = QLabel(self)
         self.blink_count_label = QLabel(self)
-        self.yawn_count_label = QLabel(self)  # CHANGES
+        self.yawn_count_label = QLabel(self)
         self.stream = CameraStream(0, self)
         self.fps_label = QLabel("FPS: 0", self)
+        self.mar = QLabel("Mouth AR:", self)
+        self.yawn_type = QLabel(self)
         self.latency_label = QLabel("Latency: 0", self)
         self.asleep_label = QLabel("ASLEEP", self)
         self.asleep_label.setStyleSheet("color:red")
@@ -318,17 +313,19 @@ class TestingWindow(QWidget):
         main_layout = QHBoxLayout()
         stats_layout = QVBoxLayout()
         frame_stats_layout = QHBoxLayout()
+        yawn_stats_layout = QHBoxLayout()
 
         self.face_toggle = QRadioButton("Face Detection", self, autoExclusive=False)
         self.hands_toggle = QRadioButton("Hands Detection", self, autoExclusive=False)
         self.blink_toggle = QRadioButton("Blink Detection", self, autoExclusive=False)
-        self.yawn_toggle = QRadioButton(
-            "Yawn Detection", self, autoExclusive=False
-        )  # CHANGES
+        self.yawn_toggle = QRadioButton("Yawn Detection", self, autoExclusive=False)
 
         self.sleep_bar = QProgressBar(self)
         self.sleep_bar.setMaximum(3 * 1000)
         self.sleep_bar.setValue(0)
+
+        yawn_stats_layout.addWidget(self.mar)
+        yawn_stats_layout.addWidget(self.yawn_type)
 
         frame_stats_layout.addWidget(self.fps_label)
         frame_stats_layout.addWidget(self.latency_label)
@@ -336,11 +333,12 @@ class TestingWindow(QWidget):
         stats_layout.addWidget(self.face_toggle)
         stats_layout.addWidget(self.hands_toggle)
         stats_layout.addWidget(self.blink_toggle)
-        stats_layout.addWidget(self.yawn_toggle)  # CHANGES
+        stats_layout.addWidget(self.yawn_toggle)
         stats_layout.addWidget(QWidget())
         stats_layout.addWidget(self.sleep_bar)
         stats_layout.addWidget(self.asleep_label)
         stats_layout.addWidget(self.blink_count_label)
+        stats_layout.addLayout(yawn_stats_layout)
         stats_layout.addLayout(frame_stats_layout)
         stats_layout.setAlignment(Qt.AlignTop)
 
@@ -360,24 +358,35 @@ class TestingWindow(QWidget):
         )
 
         self.yawn_toggle.toggled.connect(
-            lambda: self.do_yawn.emit(self.blink_toggle.isChecked())  # CHANGES
+            lambda: self.do_yawn.emit(self.yawn_toggle.isChecked())
         )
 
         self.do_face.connect(self.stream.set_do_face)
         self.do_hands.connect(self.stream.set_do_hands)
         self.do_blink.connect(self.stream.set_do_blink)
-        self.do_yawn.connect(self.stream.set_do_yawn)   #CHANGES
+        self.do_yawn.connect(self.stream.set_do_yawn)
 
-
-        self.stream.latency.connect(self.latency_update)
+        self.stream.latency_signal.connect(self.latency_update)
         self.stream.change_pixmap_signal.connect(self.update_image)
         self.stream.ear_signal.connect(self.update_ear)
         self.stream.eyes_closed_signal.connect(self.update_blink_count)
         self.stream.eyes_closed_signal.connect(self.start_closed_eyes_timer)
         self.stream.eyes_open_signal.connect(self.start_open_eyes_timer)
+        self.stream.mar_signal.connect(self.update_mar)
         self.update.connect(self.update_values)
         self.stream.start()
         self.update_values()
+
+    @pyqtSlot(float)
+    def update_mar(self, val):
+        self.mar.setText(f"Mouth AR: {val:.2f}")
+        if val > 1.:
+            yawn_type = 'YAWNING'
+        elif val < .6:
+            yawn_type = 'CLOSED'
+        else:
+            yawn_type = 'TALKING'
+        self.yawn_type.setText(yawn_type)
 
     def start_closed_eyes_timer(self):
         self.stop_all_timers()
@@ -434,7 +443,7 @@ class TestingWindow(QWidget):
 
     @pyqtSlot()
     def update_yawn_count(self):
-        self.yawn += 1  # CHANGES
+        self.yawn += 1
         self.update.emit()
 
     @pyqtSlot(float)
